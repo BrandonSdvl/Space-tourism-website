61baf9242d27c5cbf52335b615bb01d64abaa504
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-11-24 20:41:35 -0600

    Slider component

---
+++ src/components/Slider.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+transition(name="fade")
+  div(
+    @mousedown="handleMouseDown",
+    @mouseup="handleMouseUp",
+    @touchstart="handleTouchStart",
+    @touchend="handleTouchEnd"
+  )
+    slot
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Slider",
+  props: {
+    slidesLen: Number,
+    currSlide: Number,
+  },
+  emits: ["updateSlide"],
+  setup(props, { emit }) {
+    let xInitial = ref(0);
+
+    const handleMouseDown = (e) => {
+      xInitial.value = e.x;
+    };
+
+    const handleMouseUp = (e) => {
+      if (Math.abs(e.x - xInitial.value) >= 50) {
+        if (e.x - xInitial.value > 0) {
+          prev();
+        } else if (e.x - xInitial.value < 0) {
+          next();
+        }
+      }
+    };
+
+    const handleTouchStart = (e) => {
+      xInitial.value = e.changedTouches[0].clientX;
+    };
+
+    const handleTouchEnd = (e) => {
+      if (Math.abs(e.changedTouches[0].clientX - xInitial.value) >= 30) {
+        if (e.changedTouches[0].clientX - xInitial.value > 0) {
+          prev();
+        } else if (e.changedTouches[0].clientX - xInitial.value < 0) {
+          next();
+        }
+      }
+    };
+
+    const prev = () => {
+      if (props.currSlide <= 0) {
+        emit("updateSlide", props.slidesLen - 1);
+      } else {
+        emit("updateSlide", props.currSlide - 1);
+      }
+    };
+
+    const next = () => {
+      if (props.currSlide >= props.slidesLen - 1) {
+        emit("updateSlide", 0);
+      } else {
+        emit("updateSlide", props.currSlide + 1);
+      }
+    };
+
+    return {
+      handleMouseDown,
+      handleMouseUp,
+      handleTouchStart,
+      handleTouchEnd,
+      prev,
+      next,
+      xInitial,
+    };
+  },
+};
+</script>
--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -3,45 +3,63 @@ section.view--destination
   h2.view__title 01 Pick your destination
   .destination
     .destination__image-container
-      transition(name="fade", v-for="(destination, idx) in destinations")
-        img.destination__img(
-          :src="destination.images.png",
-          v-show="currDestination === idx"
-        )
+      Slider(
+        v-show="currDestination === idx",
+        v-for="(destination, idx) in destinations",
+        :slidesLen="slidesLen",
+        :currSlide="currDestination",
+        @updateSlide="updateSlide"
+      )
+        img.destination__img(:src="destination.images.png", draggable="false")
     nav.destination__nav
       ul
         li(
           v-for="(destination, idx) in destinations",
           @click="currDestination = idx"
         ) {{ destination.name }}
-    .destination__content-container
-      transition(name="fade", v-for="(destination, idx) in destinations")
-        .destination__content(v-show="currDestination === idx")
-          .destination__details
-            h2 {{ destination.name }}
-            p {{ destination.description }}
-          .destination__numbers
-            div
-              h3 Avg. distance
-              span {{ destination.distance }}
-            div
-              h3 Est. travel time
-              span {{ destination.travel }}
+    .destination__content-container.disable-selection
+      Slider.destination__content(
+        v-show="currDestination === idx",
+        v-for="(destination, idx) in destinations",
+        :slidesLen="slidesLen",
+        :currSlide="currDestination",
+        @updateSlide="updateSlide"
+      )
+        .destination__details
+          h2 {{ destination.name }}
+          p {{ destination.description }}
+        .destination__numbers
+          div
+            h3 Avg. distance
+            span {{ destination.distance }}
+          div
+            h3 Est. travel time
+            span {{ destination.travel }}
 </template>
 
 <script>
 import { useStore } from "vuex";
 import { computed, ref } from "vue";
+import Slider from "../components/Slider.vue";
 
 export default {
   name: "Destination",
+  components: {
+    Slider,
+  },
   setup() {
     const store = useStore();
     const currDestination = ref(0);
 
+    const updateSlide = (newSlide) => {
+      currDestination.value = newSlide;
+    };
+
     return {
       destinations: computed(() => store.state.destinations),
+      slidesLen: computed(() => store.state.destinations.length),
       currDestination,
+      updateSlide,
     };
   },
 };
