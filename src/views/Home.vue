644179501314a06616a7328f0851e1568f337588
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-11-29 20:03:11 -0600

    Home styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 section.view--home
-  div
-    p So, you want to travel to
-    h1 Space
-    p Let’s face it; if you want to go to space, you might as well genuinely go to
+  .home
+    p.view__title So, you want to travel to
+    h1.home__title Space
+    p.home__description.body-text Let’s face it; if you want to go to space, you might as well genuinely go to
       | outer space and not hover kind of on the edge of it. Well sit back, and relax
       | because we’ll give you a truly out of this world experience!
 
-  router-link(to="/destination") Explore
+  router-link.home__link(to="/destination") EXPLORE
 </template>
 
 <script>
