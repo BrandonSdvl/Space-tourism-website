d089aa61a56faf1a51f82e7374e2221a73ad7eae
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-12-02 21:15:37 -0600

    Header responsive tablet

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,64 +4,124 @@ nav.nav
     :src="require('../assets/shared/icon-close.svg')",
     @click="$emit('showNav')"
   )
-  ul.nav__list
+  ul.nav__list(ref="navList")
     .nav__indicator(ref="indicator")
     li.nav__item
-      router-link.nav__link(to="/", @click="$emit('showNav')")
+      router-link.nav__link(to="/", @click="handleClick")
         span.nav__number 00 &nbsp;
         | Home
     li.nav__item
-      router-link.nav__link(to="/destination", @click="$emit('showNav')")
+      router-link.nav__link(to="/destination", @click="handleClick")
         span.nav__number 01 &nbsp;
         | Destination
     li.nav__item
-      router-link.nav__link(to="/crew", @click="$emit('showNav')")
+      router-link.nav__link(to="/crew", @click="handleClick")
         span.nav__number 02 &nbsp;
         | Crew
     li.nav__item
-      router-link.nav__link(to="/technology", @click="$emit('showNav')")
+      router-link.nav__link(to="/technology", @click="handleClick")
         span.nav__number 03 &nbsp;
         | Technology
 </template>
 
 <script>
-import { ref, watch } from "vue";
+import { ref, watch, onMounted, onUnmounted } from "vue";
 import { useRoute } from "vue-router";
 
 export default {
   name: "NavBar",
   emits: ["showNav"],
-  setup() {
+  setup(props, { emit }) {
     const indicator = ref(null);
+    const navList = ref(null);
     const route = useRoute();
+    let screenWidth = window.screen.width;
+    let navX = 0;
+    let items = [];
+
+    console.log(route);
+
+    onMounted(() => {
+      window.addEventListener("resize", handleResize);
+      navX = navList.value.getBoundingClientRect().x;
+      items = document.querySelectorAll(".nav__item");
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", handleResize);
+    });
 
     watch(route, () => {
       moveIndicator();
     });
 
+    const handleResize = () => {
+      screenWidth = window.screen.width;
+      navX = navList.value.getBoundingClientRect().x;
+    };
+
+    const handleClick = () => {
+      if (screenWidth < 768) {
+        emit("showNav");
+      }
+    };
+
     const moveIndicator = () => {
-      switch (route.name) {
-        case "Home":
-          indicator.value.style.top = "0";
-          break;
-        case "Destination":
-          indicator.value.style.top = "25%";
-          break;
-        case "Crew":
-          indicator.value.style.top = "50%";
-          break;
-        case "Technology":
-          indicator.value.style.top = "75%";
-          break;
-        default:
-          indicator.value.style.top = "0";
-          break;
+      if (screenWidth < 768) {
+        switch (route.name) {
+          case "Home":
+            indicator.value.style.top = "0";
+            break;
+          case "Destination":
+            indicator.value.style.top = "25%";
+            break;
+          case "Crew":
+            indicator.value.style.top = "50%";
+            break;
+          case "Technology":
+            indicator.value.style.top = "75%";
+            break;
+          default:
+            indicator.value.style.top = "0";
+            break;
+        }
+      } else {
+        switch (route.name) {
+          case "Home":
+            indicator.value.style.left = `${
+              items[0].getBoundingClientRect().x - navX
+            }px`;
+            indicator.value.style.width = `${items[0].clientWidth}px`;
+            break;
+          case "Destination":
+            indicator.value.style.left = `${
+              items[1].getBoundingClientRect().x - navX
+            }px`;
+            indicator.value.style.width = `${items[1].clientWidth}px`;
+            break;
+          case "Crew":
+            indicator.value.style.left = `${
+              items[2].getBoundingClientRect().x - navX
+            }px`;
+            indicator.value.style.width = `${items[2].clientWidth}px`;
+            break;
+          case "Technology":
+            indicator.value.style.left = `${
+              items[3].getBoundingClientRect().x - navX
+            }px`;
+            indicator.value.style.width = `${items[3].clientWidth}px`;
+            break;
+          default:
+            break;
+        }
       }
     };
 
     return {
       indicator,
+      navList,
       moveIndicator,
+      handleClick,
     };
   },
 };
